12fee688c082792481d2cfbfcb9cf64220809a29
cd-JL <jmasterl@live.com>
2023-11-12 16:19:17 -0700

    first push, messed up the functionality. does not work

---
+++ MobileDLab3/index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
